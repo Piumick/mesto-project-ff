8d7ba2fa0698b9b92f81e9b72830cec9ca598d1b
piumick <piumickk@gmail.com>
2024-05-14 13:51:17 +0300

    fijgk

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function addCard(link, title, func) {
 
-// @todo: Функция удаления карточки
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button')
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = title;
+  
+  deleteButton.addEventListener('click', function(){
+    deleteCard(deleteButton)
+  })
+  
+  return cardElement
+}
+const deleteCard = function(deleteButton){
+  const deleteItem = deleteButton.closest('.card');
+    deleteItem.remove();
+}
 
-// @todo: Вывести карточки на страницу
+for(let i in initialCards){
+  cardList.append(
+  addCard(initialCards[i].link, initialCards[i].name, deleteCard))
+}
+
