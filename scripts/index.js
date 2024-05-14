0ed6f5f926815e07e80fca97415389192087eac6
piumick <piumickk@gmail.com>
2024-05-14 17:06:08 +0300

    jdkkls

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
+const cardList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-const cardList = document.querySelector('.places__list');
+function addCard(link, title, deleteFunc) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardImage.src = link;
+  cardImage.alt = title;
+  cardTitle.textContent = title;
 
-function addCard(link, title, func) {
+  deleteButton.addEventListener("click", function () {
+    deleteFunc(cardElement);
+  });
 
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  const deleteButton = cardElement.querySelector('.card__delete-button')
-  cardElement.querySelector('.card__image').src = link;
-  cardElement.querySelector('.card__title').textContent = title;
-  
-  deleteButton.addEventListener('click', function(){
-    deleteCard(deleteButton)
-  })
-  
-  return cardElement
+  return cardElement;
 }
-const deleteCard = function(deleteButton){
-  const deleteItem = deleteButton.closest('.card');
-    deleteItem.remove();
-}
+const deleteCard = function (deleteElement) {
+  deleteElement.remove();
+};
 
-for(let i in initialCards){
+for (let i in initialCards) {
   cardList.append(
-  addCard(initialCards[i].link, initialCards[i].name, deleteCard))
+    addCard(initialCards[i].link, initialCards[i].name, deleteCard)
+  );
 }
-
